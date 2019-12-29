aef818cfe5f2a087c891aa28a8faae5c0b539fd0
Near <250327864@qq.com>
2019-12-29 15:29:46 +0800

    fix
    Signed-off-by: Near <250327864@qq.com>

--- services/Youxiduo/Order/Model/Order.php
+++ services/Youxiduo/Order/Model/Order.php
@@ -30,7 +30,7 @@ final class Order extends Model implements IModel
         $tb = self::db();
         if(isset($search['urid']) && !empty($search['urid'])) $tb = $tb->where('urid','=',$search['urid']);
         if(isset($search['orderNo']) && !empty($search['orderNo'])) $tb = $tb->where('orderNo','like','%'.$search['orderNo'].'%');
-        if(isset($search['status']) && !empty($search['status'])) $tb = $tb->where('status','in','('.implode(',',$search['status']).')');
+        if(isset($search['status']) && !empty($search['status'])) $tb = $tb->whereIn('status',$search['status']);
         return $tb->orderBy('orid','desc')->forPage($pageIndex,$pageSize)->get();
     }
 
