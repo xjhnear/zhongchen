e23ae47279ae4f7445f11441bdcc1ad3de93f984
Near <250327864@qq.com>
2020-03-08 19:34:25 +0800

    fix
    Signed-off-by: Near <250327864@qq.com>

--- apps/admin/modules/config/controllers/ConfigController.php
+++ apps/admin/modules/config/controllers/ConfigController.php
@@ -26,12 +26,14 @@ class ConfigController extends BackendController
     {
         $data = array();
         $data['data'] = Config::getInfoByType(1);
+        $data['data2'] = Config::getInfoByType(2);
+        $data['data3'] = Config::getInfoByType(3);
         return $this->display('config_info', $data);
     }
 
     public function postSave()
     {
-        $input = Input::only('id', 'content');
+        $input = Input::only('id', 'content','id2', 'content2','id3', 'content3');
 
         if ($input['id'] > 0) {
             $data['id'] = $input['id'];
@@ -39,7 +41,21 @@ class ConfigController extends BackendController
         $data['type'] = 1;
         $data['content'] = $input['content'];
         $result = Config::saveInfo($data);
-        
+
+        if ($input['id2'] > 0) {
+            $data2['id'] = $input['id2'];
+        }
+        $data2['type'] = 2;
+        $data2['content'] = $input['content2'];
+        $result2 = Config::saveInfo($data2);
+
+        if ($input['id3'] > 0) {
+            $data3['id'] = $input['id3'];
+        }
+        $data3['type'] = 3;
+        $data3['content'] = $input['content3'];
+        $result3 = Config::saveInfo($data3);
+
         if ($result) {
             return $this->redirect('config/config/info')->with('global_tips', '保存成功');
         } else {
