95a2a2c9145cee736b3b134a784d757fe221f1f8
near <250327864@qq.com>
2020-04-01 15:20:05 +0800

    fix
    Signed-off-by: near <250327864@qq.com>

--- services/Youxiduo/Helper/Utility.php
+++ services/Youxiduo/Helper/Utility.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Log;
 use App;
 use Illuminate\Support\Facades\Config;
 use Youxiduo\Helper\MyHelpLx;
-use AlibabaCloud\Client\AlibabaCloud;
-use AlibabaCloud\Client\Exception\ClientException;
-use AlibabaCloud\Client\Exception\ServerException;
+//use AlibabaCloud\Client\AlibabaCloud;
+//use AlibabaCloud\Client\Exception\ClientException;
+//use AlibabaCloud\Client\Exception\ServerException;
+use Aliyun\DySDKLite\SignatureHelper;
 
 class Utility
 {
@@ -569,6 +570,63 @@ class Utility
     }
 
     public static function sendVerifyAllSMS($mobile,$code,$sms=true)
+    {
+        $params = array ();
+
+        // *** 需用户填写部分 ***
+        // fixme 必填：是否启用https
+        $security = false;
+
+        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+        $accessKeyId = Config::get('sms.AccessKeyId');
+        $accessKeySecret = Config::get('sms.AccessKeySecret');
+
+        // fixme 必填: 短信接收号码
+        $params["PhoneNumbers"] = $mobile;
+
+        // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $params["SignName"] = Config::get('sms.SignName');
+
+        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = Config::get('sms.TemplateCode');
+
+        // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+        $params['TemplateParam'] = Array (
+            "code" => $code
+        );
+
+        // fixme 可选: 设置发送短信流水号
+//        $params['OutId'] = "12345";
+
+        // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+//        $params['SmsUpExtendCode'] = "1234567";
+
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendSms",
+                "Version" => "2017-05-25",
+            )),
+            $security
+        );
+print_r(json_encode($content));exit;
+        return $content;
+    }
+
+    public static function sendVerifyAllSMS1($mobile,$code,$sms=true)
     {
         AlibabaCloud::accessKeyClient(Config::get('sms.AccessKeyId'), Config::get('sms.AccessKeySecret'))
             ->regionId('cn-hangzhou')
